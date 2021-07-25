8597c6bef49b2ea0d1983fd4dcda487fbcd92d81
ElTacitos <hugo.locatelli1@gmail.com>
2021-07-25 18:29:47 +0200

    added winning cell coloration

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -2,7 +2,7 @@
   <div class="board">
     <div class="line" v-for="line in board" :key="line.id">
       <div class="row" v-for="cell in line" :key="cell.id" v-on:click="cross(cell)"
-            :class="{ notCheck: !cell.checked && !gameEnded}">
+            :class="{ notCheck: !cell.checked && !gameEnded, winCell: cell.winning}">
         <p class="cell">{{cell.symbol}}</p>
       </div>
     </div>
@@ -18,9 +18,12 @@ export default {
   watch:{
     //Use needToReset to know when to reset
     needToReset: function (){
-      this.board = [[{symbol: ' ', checked: false},{symbol: ' ', checked: false},{symbol: ' ', checked: false}],
-        [{symbol: ' ', checked: false},{symbol: ' ', checked: false},{symbol: ' ', checked: false}],
-        [{symbol: ' ', checked: false},{symbol: ' ', checked: false},{symbol: ' ', checked: false}]];
+      this.board = [[{symbol: ' ', checked: false, winning: false},{symbol: ' ', checked: false, winning: false},
+        {symbol: ' ', checked: false, winning: false}],
+        [{symbol: ' ', checked: false, winning: false},{symbol: ' ', checked: false, winning: false},
+          {symbol: ' ', checked: false, winning: false}],
+        [{symbol: ' ', checked: false, winning: false},{symbol: ' ', checked: false, winning: false},
+          {symbol: ' ', checked: false, winning: false}]];
       this.placed = 0;
       this.gameEnded = false;
       this.$emit('resetDone');
@@ -29,9 +32,12 @@ export default {
   data() {
     return {
       board: [
-        [{symbol: ' ', checked: false},{symbol: ' ', checked: false},{symbol: ' ', checked: false}],
-        [{symbol: ' ', checked: false},{symbol: ' ', checked: false},{symbol: ' ', checked: false}],
-        [{symbol: ' ', checked: false},{symbol: ' ', checked: false},{symbol: ' ', checked: false}],
+          [{symbol: ' ', checked: false, winning: false},{symbol: ' ', checked: false, winning: false},
+        {symbol: ' ', checked: false, winning: false}],
+        [{symbol: ' ', checked: false, winning: false},{symbol: ' ', checked: false, winning: false},
+          {symbol: ' ', checked: false, winning: false}],
+        [{symbol: ' ', checked: false, winning: false},{symbol: ' ', checked: false, winning: false},
+          {symbol: ' ', checked: false, winning: false}]
       ],
       placed: 0,
       gameEnded: false,
@@ -59,12 +65,18 @@ export default {
             && (this.board[i][1].symbol == this.board[i][2].symbol)) {
           this.gameEnded = true;
           this.$emit('endGame',this.board[i][0].symbol);
+          this.board[i][0].winning = true;
+          this.board[i][1].winning = true;
+          this.board[i][2].winning = true;
           return;
         }
         if ((this.board[0][i].symbol != " ") && (this.board[0][i].symbol == this.board[1][i].symbol)
             && (this.board[1][i].symbol == this.board[2][i].symbol)) {
           this.gameEnded = true;
           this.$emit('endGame',this.board[0][i].symbol);
+          this.board[0][i].winning = true;
+          this.board[1][i].winning = true;
+          this.board[2][i].winning = true;
           return;
         }
       }
@@ -72,12 +84,18 @@ export default {
           && (this.board[1][1].symbol == this.board[2][2].symbol)) {
         this.gameEnded = true;
         this.$emit('endGame',this.board[1][1].symbol);
+        this.board[0][0].winning = true;
+        this.board[1][1].winning = true;
+        this.board[2][2].winning = true;
         return;
       }
       if ((this.board[1][1].symbol != " ") && (this.board[0][2].symbol == this.board[1][1].symbol)
           && (this.board[1][1].symbol == this.board[2][0].symbol)) {
         this.gameEnded = true;
         this.$emit('endGame',this.board[1][1].symbol);
+        this.board[0][2].winning = true;
+        this.board[1][1].winning = true;
+        this.board[2][0].winning = true;
         return;
       }
       if (this.placed == 9) {
@@ -116,12 +134,15 @@ export default {
   color: #E76F51;
 }
 .notCheck:hover{
-  background-color: #30596a;
+  background-color: #345E6F;
+}
+.winCell{
+  background-color: #345E6F;
 }
 @media only screen and (max-width: 600px) {
   .row{
     border-width: 2px;
-    border-color: #1A2F37;
+    border-color: #23404B;
     border-style: solid;
     display: flex;
     flex-direction: column;
